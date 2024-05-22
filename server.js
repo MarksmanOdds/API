c2975ffb85902a084c236b8c79c7975576457d89
Luke Dasios <lukedasios@321@gmail.com>
2024-05-22 13:44:23 -0400

    total endpoint complete

--- server.js
+++ server.js
@@ -15,19 +15,13 @@ async function closeMongoDBConnection() {
 }
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/odds/:league/:sportsbooks/:market", async (req, res) => {
-  const { league, sportsbooks, market } = req.params;
-  if (market !== "moneyline") {
-    return res
-      .status(400)
-      .send("This endpoint currently supports only 'moneyline' market.");
-  }
-
+app.get("/moneyline/:league/:sportsbooks", async (req, res) => {
+  const { league, sportsbooks } = req.params;
   const sportsbookArray = sportsbooks.split(",");
 
   try {
@@ -111,7 +105,9 @@ app.get("/odds/:league/:sportsbooks/:market", async (req, res) => {
                 $filter: {
                   input: "$moneylines",
                   as: "line",
-                  cond: { $eq: ["$$line.t1_moneyline", "$best_t1_moneyline"] },
+                  cond: {
+                    $eq: ["$$line.t1_moneyline", "$best_t1_moneyline"],
+                  },
                 },
               },
               in: "$$this.sportsbook",
@@ -123,7 +119,9 @@ app.get("/odds/:league/:sportsbooks/:market", async (req, res) => {
                 $filter: {
                   input: "$moneylines",
                   as: "line",
-                  cond: { $eq: ["$$line.t2_moneyline", "$best_t2_moneyline"] },
+                  cond: {
+                    $eq: ["$$line.t2_moneyline", "$best_t2_moneyline"],
+                  },
                 },
               },
               in: "$$this.sportsbook",
@@ -141,6 +139,147 @@ app.get("/odds/:league/:sportsbooks/:market", async (req, res) => {
     res.json(events);
   } catch (error) {
     console.error("Failed to fetch moneyline odds with aggregation:", error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+app.get("/total/:league/:sportsbooks", async (req, res) => {
+  const { league, sportsbooks } = req.params;
+  const sportsbookArray = sportsbooks.split(",");
+
+  try {
+    await connectToMongoDB();
+
+    const pipeline = [
+      {
+        $match: {
+          league: league,
+          sportsbook: { $in: sportsbookArray },
+          $or: [{ t1_total: { $ne: null } }, { t2_total: { $ne: null } }],
+        },
+      },
+      {
+        $lookup: {
+          from: "upcomings",
+          localField: "league",
+          foreignField: "league",
+          as: "upcomingDetails",
+        },
+      },
+      {
+        $unwind: {
+          path: "$upcomingDetails",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $group: {
+          _id: {
+            t1_name: "$t1_name",
+            t2_name: "$t2_name",
+            sportsbook: "$sportsbook",
+          },
+          t1_total: { $first: "$t1_total" },
+          t2_total: { $first: "$t2_total" },
+          t1_total_line: { $first: "$t1_total_line" },
+          t2_total_line: { $first: "$t2_total_line" },
+          date: { $first: "$upcomingDetails.date" },
+        },
+      },
+      {
+        $group: {
+          _id: { t1_name: "$_id.t1_name", t2_name: "$_id.t2_name" },
+          totals: {
+            $push: {
+              sportsbook: "$_id.sportsbook",
+              t1_total: "$t1_total",
+              t2_total: "$t2_total",
+              t1_total_line: "$t1_total_line",
+              t2_total_line: "$t2_total_line",
+            },
+          },
+          date: { $max: "$date" },
+        },
+      },
+      {
+        $addFields: {
+          best_t1_total: { $max: "$totals.t1_total" },
+          best_t2_total: { $max: "$totals.t2_total" },
+        },
+      },
+      {
+        $addFields: {
+          best_t1_total_sportsbooks: {
+            $map: {
+              input: {
+                $filter: {
+                  input: "$totals",
+                  as: "total",
+                  cond: { $eq: ["$$total.t1_total", "$best_t1_total"] },
+                },
+              },
+              as: "filteredTotal",
+              in: "$$filteredTotal.sportsbook",
+            },
+          },
+          best_t2_total_sportsbooks: {
+            $map: {
+              input: {
+                $filter: {
+                  input: "$totals",
+                  as: "total",
+                  cond: { $eq: ["$$total.t2_total", "$best_t2_total"] },
+                },
+              },
+              as: "filteredTotal",
+              in: "$$filteredTotal.sportsbook",
+            },
+          },
+          best_t1_total_info: {
+            $first: {
+              $filter: {
+                input: "$totals",
+                as: "total",
+                cond: { $eq: ["$$total.t1_total", "$best_t1_total"] },
+              },
+            },
+          },
+          best_t2_total_info: {
+            $first: {
+              $filter: {
+                input: "$totals",
+                as: "total",
+                cond: { $eq: ["$$total.t2_total", "$best_t2_total"] },
+              },
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          t1_name: "$_id.t1_name",
+          t2_name: "$_id.t2_name",
+          totals: 1,
+          date: 1,
+          best_t1_total: 1,
+          best_t2_total: 1,
+          best_t1_total_line: "$best_t1_total_info.t1_total_line",
+          best_t2_total_line: "$best_t2_total_info.t2_total_line",
+          best_t1_total_sportsbooks: 1,
+          best_t2_total_sportsbooks: 1,
+        },
+      },
+      {
+        $sort: { t1_name: 1, t2_name: 1 },
+      },
+    ];
+
+    const events = await EventModel.aggregate(pipeline);
+    await closeMongoDBConnection();
+    res.json(events);
+  } catch (error) {
+    console.error("Failed to fetch total lines with aggregation:", error);
     res.status(500).send("Internal server error");
   }
 });
